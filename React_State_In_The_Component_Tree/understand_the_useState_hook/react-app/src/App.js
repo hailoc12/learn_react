e1ce7cb21dedae3c5faaf5043e8460ca9ee738d7
hailoc12 <hailoc12@gmail.com>
2022-07-05 07:06:52 +0700

    update

---
+++ React_State_In_The_Component_Tree/understand_the_useState_hook/react-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react"
+
+function App() {
+  const [ emotion, setEmotion ] = useState("Happy")
+
+
+  return (
+    <div className="App">
+      <h1>Current emotion is {emotion}</h1>    
+      <button onClick={() => setEmotion("Sad")}>
+        Sad
+      </button>
+      <button onClick={() => setEmotion("Excited")}>
+        Excited
+      </button>
+    </div>
+  );
+}
+
+export default App;
